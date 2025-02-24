6423f8291b3c07df569f01db85a204d2e434c7d3
arafat11223 <arafat221a@gmail.com>
2025-02-24 16:07:42 +0600

    tui ki aslei choooder

---
+++ coding/programming hero/javascript/loop/for__of.js
@@ -0,0 +1,19 @@
+const ele = [2, 3, 5, 67, 5];
+for (let num of ele) {
+    console.log(num);
+}
+for (let num of ele) {
+    console.log("value :", num);
+}
+
+const string ="hello";
+for(let char of string)
+{
+    console.log(char);
+}
+
+let fruits=["banana","apple","orange"];
+for(let f of fruits)
+{
+    console.log(f);
+}
